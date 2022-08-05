020e4b87b8c68bf077fbfa82b90d5d1ff68b9e54
Krisztian Barta <bartariki@gmail.com>
2022-08-05 15:34:58 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Makuka from './images/makuka.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <> <nav className='border-b-2  flex-row justify-end' style={{fontFamily:"cursive"}}>
+      <ul className='flex m-2 space-x-7'>
+        <li className='flex-grow-1'><a className='hover:bg-slate-200 rounded-md no-underline text-black'> Torma Gellért </a></li>
+        <li className='
+         hover:bg-slate-200 rounded-md'>Főoldal</li>
+        <li className=' hover:bg-slate-200 rounded-md'>Írásaim</li>
+        <li className=' hover:bg-slate-200 rounded-md'>Tanfolyamok</li>
+        <li className=' hover:bg-slate-200 rounded-md'>Rólam</li>
+      </ul>
+    </nav>
+    <img src={Makuka} alt='makuka' style={{width: "100%", height:"450px"}}/>
+
+    <div className='flex m-2 justify-center text-3xl'>Újdonságok, Hírek</div>
+    </>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/* eslint-disable no-undef */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        '3xl': '1800px',
+        ...defaultTheme.screens,
+      }
+    },
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+  ],
+}
